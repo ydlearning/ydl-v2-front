04ff12574e40d5bcb4b1b0088f179bffa043fdad
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-04-26 00:39:46 +0200

    Revert "add vuetify"
    This reverts commit bf425ae728b9e3bc058d760d932aeb598f59dad8.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  name: "home",
+  components: {
+    HelloWorld
   }
+};
 </script>
