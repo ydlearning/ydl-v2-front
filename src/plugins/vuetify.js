d21324c1fc0e1116610167e77d0218d5ce215ac6
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-04-29 22:59:24 +0200

    add prettifier settings

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
-import de from 'vuetify/es5/locale/de'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
+import de from "vuetify/es5/locale/de";
 
 Vue.use(Vuetify, {
-  iconfont: 'mdi',
-  lang: {
-    locales: { de },
-    current: 'de'
-  },
-})
+    iconfont: "mdi",
+    lang: {
+        locales: { de },
+        current: "de"
+    }
+});
