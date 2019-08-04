c78ade8351e346622a798b602e70782b5f3d64b4
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-08-04 22:45:21 +0200

    rebuild site

--- buildsite.ps1
+++ buildsite.ps1
@@ -1,3 +1,10 @@
-﻿pnpm run lint;
+﻿Write-Host -ForegroundColor Yellow "Build site - script";
+
+Write-Host -ForegroundColor Yellow "1: Run lint";
+pnpm run lint;
+
+Write-Host -ForegroundColor Yellow "2: Run build";
 pnpm run build;
+
+Write-Host -ForegroundColor Yellow "3: Generate documentation";
 vuese gen;
