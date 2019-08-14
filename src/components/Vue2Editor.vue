936a8f1e7ebbf843bf0e2a2dd300b4ef967abd7b
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-14 11:23:22 +0200

    add Vue2Editor component
    its a wrapper for the vue2-editor plugin

---
+++ src/components/Vue2Editor.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    v-card(flat tile)
+        v-card-text
+            vue-editor(v-model="content")
+</template>
+
+<script>
+import { VueEditor } from "vue2-editor/dist/vue2-editor.core.js";
+
+export default {
+    name: "Vue2Editor",
+    data: () => ({
+        content: "<h1>initial content</h1>"
+    }),
+    components: { VueEditor }
+};
+</script>
+
+<style lang="sass">
+@import "~vue2-editor/dist/vue2-editor.css"
+
+/* Import the Quill styles you want */
+@import '~quill/dist/quill.core.css'
+@import '~quill/dist/quill.snow.css'
+
+.ql-snow.ql-toolbar
+    background-color: gray
+</style>
