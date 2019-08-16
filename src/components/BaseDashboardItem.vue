19a53d48f9ea63f42265da3fa8ee209efe8a5bb8
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-17 00:15:28 +0200

    add BaseDashboardItem component

---
+++ src/components/BaseDashboardItem.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    v-card
+        v-card-title
+            span.title {{ title }}
+            v-spacer
+            v-btn(icon text color="blue")
+                v-icon {{ icon }}
+        slot
+</template>
+
+<script>
+export default {
+    name: "BaseDashboardItem",
+    props: {
+        title: {
+            type: String,
+            default: "<Title>"
+        },
+        icon: {
+            type: String,
+            default: "mdi-window-restore"
+        }
+    }
+};
+</script>
+
+<style lang="sass"></style>
