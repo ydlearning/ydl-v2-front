c58c53a51e62e38b60b0181682bb10ea28de4cbc
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-16 13:30:24 +0200

    add TheNotification component

---
+++ src/components/NotificationList.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    v-list(dense)
+        v-list-item-group
+            template(v-for="(notification, index) in notifications")
+                NotificationListItem(
+                    :subject="notification.subject"
+                    :headline="notification.headline"
+                    :date="notification.date"
+                )
+                //- only the render the divider between list items not at the end
+                v-divider(v-if="index < notifications.length - 1")
+</template>
+<
+<script>
+import NotificationListItem from "@/components/NotificationListItem";
+
+import json from "@/data/dashboard/notifications.json";
+
+export default {
+    name: "NotificationList",
+    components: {
+        NotificationListItem
+    },
+    data() {
+        return {
+            notifications: []
+        };
+    },
+    mounted() {
+        this.notifications = json.data.allNotifications;
+    }
+};
+</script>
+
+<style lang="sass"></style>
---
+++ src/components/NotificationListItem.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    v-list-item
+        v-list-item-content
+            v-list-item-title {{subject}}
+            v-list-item-subtitle {{headline}}
+        v-list-item-action
+            v-list-item-action-text {{new Date(date).format('MM/DD/YYYY')}}
+            v-list-item-action-text {{new Date(date).format('hh:mm A')}}
+</template>
+
+<script>
+export default {
+    props: {
+        subject: {
+            type: String,
+            default: "subject"
+        },
+        headline: {
+            type: String,
+            default: "headline"
+        },
+        date: {
+            type: String,
+            default: "date"
+        }
+    }
+};
+</script>
