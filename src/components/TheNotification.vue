99a5884f391edcd25f13d16b5815138f0113c3aa
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-16 13:30:24 +0200

    add TheNotification component

---
+++ src/components/TheNotification.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    v-card
+        v-card-title
+            span.title Notifications
+            v-spacer
+            v-btn(icon text color="blue")
+                v-icon mdi-window-restore
+        NotificationList
+</template>
+
+<script>
+import NotificationList from "@/components/NotificationList";
+
+export default {
+    name: "TheNotification",
+    components: {
+        NotificationList
+    }
+};
+</script>
+
+<style lang="sass"></style>
