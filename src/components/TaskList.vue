63bd15efb8eb95bdbfc6b24f687dfd49998a41b8
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-17 00:16:15 +0200

    add task components

---
+++ src/components/TaskList.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    v-list(dense)
+        template(v-for="(task, index) in tasks")
+            TaskListItem(
+                :courseName="task.course_name"
+                :name="task.name"
+                :dueTo="task.due_to"
+                :type="task.type"
+            )
+            v-divider(v-if="index < tasks.length - 1")
+</template>
+
+<script>
+import TaskListItem from "@/components/TaskListItem";
+
+import json from "@/data/dashboard/tasks.json";
+
+export default {
+    name: "TaskList",
+    data() {
+        return {
+            tasks: []
+        };
+    },
+    components: {
+        TaskListItem
+    },
+    mounted() {
+        this.tasks = json.data.allTasks;
+    }
+};
+</script>
+
+<style lang="sass"></style>
