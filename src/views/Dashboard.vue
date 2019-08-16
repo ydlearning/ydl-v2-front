69619458a993d3f8599073c4335cf904e1717f2e
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-17 00:19:27 +0200

    modify Dashboard view to use the Tasks component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,25 +5,35 @@
                 v-row(no-gutters)
                     v-col.pb-2.pt-4.px-4
                         v-card.pa-2(tile)
+                            //- layout end
                             v-toolbar.orange(flat) Profile
                             p LLl
                 v-row(no-gutters)
                     v-col.pt-2.pb-4.px-4
-                        v-card.pa-2(tile)
+                        v-card.pa-2(tile flat color="transparent")
+                            //- layout end
                             v-card-title Favorite Courses
             v-col.pa-4(cols=12 sm=3)
-                v-card.pa-2.fill-height(tile)
-                    TheNotification
+                v-card.pa-0.fill-height(tile flat color="transparent")
+                    //- layout 
+                    v-row.ma-0
+                        v-col
+                            TheNotification
+                        v-col
+                            TheTask
+                            
                     
                             
 </template>
 
 <script>
 import TheNotification from "@/components/TheNotification";
+import TheTask from "@/components/TheTask";
 
 export default {
     components: {
-        TheNotification
+        TheNotification,
+        TheTask
     },
     mounted() {
         console.debug(this.$route.name);
