30004c74e1cb630cae4e0ac4089e4f03fd5a72c8
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-20 16:04:59 +0200

    add TheFavoriteCourse component

---
+++ src/components/TheFavoriteCourse.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    BaseDashboardItem(
+        title="Favorite Courses"
+    )
+        v-container(fluid)
+            v-row
+                v-col
+                    v-toolbar
+                        v-text-field(
+                            prepend-icon="mdi-magnify"
+                            hide-details
+                            clearable
+                            label="Search"
+                            value="Mana"
+                        )
+            v-row
+                v-col(v-for="n in 8" :key="n" cols=12 sm=6 lg=3)
+                    FavoriteCourseItem(
+                        header="Frontend Course"
+                    )
+
+</template>
+
+<script>
+import BaseDashboardItem from "@/components/BaseDashboardItem";
+import FavoriteCourseItem from "@/components/FavoriteCourseItem";
+
+export default {
+    name: "TheFavoriteCourse",
+    components: {
+        BaseDashboardItem,
+        FavoriteCourseItem
+    }
+};
+</script>
+
+<style lang="sass"></style>
