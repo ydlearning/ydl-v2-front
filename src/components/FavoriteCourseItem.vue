d2b803f9d6d3b495cfa136e675454f98f040bb34
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-20 16:04:22 +0200

    add FavoriteCourseItem component

---
+++ src/components/FavoriteCourseItem.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    v-card
+        v-img(
+            :src="require('../data/dashboard/images/' + Math.floor((Math.random() * 7) + 0) + '.jpg')"
+            height=130
+        )
+            v-card-title.align-start.fill-height.justify-end
+                v-btn(icon)
+                    v-icon mdi-message
+        v-card-actions {{ header }}
+            v-spacer
+            v-btn(icon small)
+                v-icon(size=16) mdi-star
+</template>
+
+<script>
+export default {
+    name: "FavoriteCourseItem",
+    props: {
+        header: {
+            type: String,
+            default: "header"
+        }
+    }
+};
+</script>
+
+<style lang="sass"></style>
