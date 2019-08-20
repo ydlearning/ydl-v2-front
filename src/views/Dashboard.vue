82e7b1f9c22edca9389f458a400856448e0a7bd1
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-20 16:06:14 +0200

    refactor use TheFavoriteCourse component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,74 +1,24 @@
 <template lang="pug">
     v-container.pa-0(fluid)
-        v-row(no-gutters)
+        v-row.ma-0
             v-col(cols=12 sm=9)
-                v-row(no-gutters)
-                    v-col.pb-2.pt-4.px-4
-                        v-card.pa-2(tile)
-                            //- layout end
-                            v-toolbar.orange(flat) Profile
-                            p LLl
-                v-row(no-gutters)
-                    v-col.pt-2.pb-4.px-4
-                        v-card.pa-2(tile flat color="transparent")
-                            //- layout end
-                            v-card-title Favorite Courses
-                            v-toolbar
-                                v-text-field(
-                                    prepend-icon="mdi-magnify"
-                                    hide-details
-                                    clearable
-                                    label="Search"
-                                    value="Mana"
-                                )
-                            v-container(fluid)
-                                v-row
-                                    v-col(v-for="n in 4" :key="n" cols=12 sm=6 lg=3)
-                                        v-card(tile)
-                                            v-img(
-                                                :src="require('../data/dashboard/images/' + Math.floor((Math.random() * 7) + 0) + '.jpg')"
-                                                height=130
-                                            )
-                                            v-card-actions
-                                                span.blue--text.font-weight-black Mana
-                                                span gement, Practice and Law
-                                                v-spacer
-                                                v-btn(icon small)
-                                                    v-icon(size=16) mdi-star
-                                v-row
-                                    v-col(v-for="n in 4" :key="n" cols=12 sm=6 lg=3)
-                                        v-card(tile )
-                                            v-img(
-                                                :src="require('../data/dashboard/images/' + Math.floor((Math.random() * 7) + 0) + '.jpg')"
-                                                height=130
-                                            )
-                                            v-card-actions
-                                                span Management, Practice and Law
-                                                v-spacer
-                                                v-btn(icon small)
-                                                    v-icon(size=16) mdi-star
+                TheFavoriteCourse
 
-            v-col.pa-4(cols=12 sm=3)
-                v-card.pa-0.fill-height(tile flat color="transparent")
-                    //- layout 
-                    v-row.ma-0
-                        v-col
-                            TheNotification
-                        v-col
-                            TheTask
-                            
-                    
-                            
+            v-col(cols=12 sm=3)
+                TheNotification.mb-4
+                TheTask       
 </template>
 
 <script>
 import TheNotification from "@/components/TheNotification";
 import TheTask from "@/components/TheTask";
+import TheFavoriteCourse from "@/components/TheFavoriteCourse";
 
 export default {
     components: {
         TheNotification,
-        TheTask
+        TheTask,
+        TheFavoriteCourse
     },
     mounted() {
         console.debug(this.$route.name);
