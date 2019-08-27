d552cb5a9db568e09ebfed1539d23c1f8558731d
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-08-28 00:34:21 +0200

    refactor(thepasswordnewfield): rename names for better understanding
    + add label name - remove placeholder

--- src/components/ThePasswordNewField.vue
+++ src/components/ThePasswordNewField.vue
@@ -1,27 +1,51 @@
 <template lang="pug">
-//- Password:
-//- @v-validate: required | min:8 | max:128 
-//- @required: true
-v-text-field(
-    v-model='password'
-    v-validate="{ required: true, min: minCounterPassword, max: maxCounterPassword, regex: regexExpression }" 
-    :counter="maxCounterPassword"
-    name="password" 
-    :data-vv-as="passwordFieldName"
-    :placeholder="passwordFieldPlaceholder" 
-    :class="{'is-danger': errors.has('password')}" 
-    ref="password" 
-    :error-messages="errors.collect('password')"
-    clearable=true
-    :append-icon=" showEyePassword ? 'mdi-eye' : 'mdi-eye-off'"
-    @click:append="showEyePassword = !showEyePassword"
-    :type="showEyePassword ? 'text' : 'password'")
+div
+	//- Password New / Repeat:
+	//- @v-validate: required
+	//- @required: true
+	v-text-field(
+		v-model="password"
+		v-on:input="check_password"
+		v-validate="{ required: true, min: minCounterPassword, max: maxCounterPassword, regex: regexExpression }"
+		:counter="maxCounterPassword" 
+		name="password"
+		:data-vv-as="fieldName"
+		:label="fieldLabel"
+		:class="{'is-danger': errors.has('password')}" 
+		ref="password" 
+		:error-messages="errors.collect('password')"
+		:clearable="fieldClearable"
+		:append-icon=" showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+		@click:append="showPassword = !showPassword"
+		:type="showPassword ? 'text' : 'password'"
+		@input="$emit('input',$event)"
+	)
+
+	v-progress-linear(
+		v-model='progress'
+		:value="progress" 
+		:color="color" 
+		height="7"
+		active)
+
+	v-alert.ma-1(dense type="info" text) Password has to be 
+		span.font-weight-bold at least:
+		//- iterate over each condition for rendering as alert
+		- var items = ["• " + minCounterPassword + " characters long", "• include 1 character", "• include 1 number", "• 1 special character, allowed: $&+:;=?@#'<>.^*()%!-{}"]
+		each item, index in items
+			//- "+index+" escapes the javascript input for the :type variable so the pug variable can be inserted
+			v-alert.caption.ma-1.pa-1(:type="corrections["+index+"]" text)= item    
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-    name: "ThePasswordField",
+    name: "ThePasswordNewField",
     props: {
+        value: {
+            type: String,
+            default: ""
+        },
         minCounterPassword: {
             type: Number,
             default: 0,
@@ -37,32 +61,87 @@ export default {
             default: "",
             required: true
         },
-        passwordFieldName: {
+        regexExpressionSpecialChars: {
+            type: RegExp,
+            default: "",
+            required: true
+        },
+        fieldName: {
             type: String,
             default: "",
             required: true
         },
-        passwordFieldPlaceholder: {
+        fieldLabel: {
             type: String,
             default: "",
             required: true
         },
-        showEyePassword: {
+        showPassword: {
             type: Boolean,
             default: false,
             required: false
         },
-        passwordFieldClearable: {
+        fieldClearable: {
             type: Boolean,
-            default: false,
+            default: true,
             required: false
         }
     },
     data() {
         return {
-            // regexExpression: /^([a-zA-Z0-9$&+,:;=?@#'<>.^*()%!-]+)$/
-            password: ""
+            password: "",
+            corrections: ["error", "error", "error", "error"]
         };
+    },
+    methods: {
+        check_password: function(value) {
+            //- check if value is defined
+            if (!value) {
+                return;
+            }
+
+            //- check password length
+            if (value.length >= 8) {
+                this.corrections[0] = "success";
+            } else {
+                this.corrections[0] = "error";
+            }
+
+            //- check for character
+            if (/[a-zA-Z]/.test(value)) {
+                this.corrections[1] = "success";
+            } else {
+                this.corrections[1] = "error";
+            }
+
+            //- check for number
+            if (/\d/.test(value)) {
+                this.corrections[2] = "success";
+            } else {
+                this.corrections[2] = "error";
+            }
+
+            //- check for special character
+            if (this.regexExpressionSpecialChars.test(value)) {
+                this.corrections[3] = "success";
+                // [+-@!#%$^?!:.;,()[] {}]
+            } else {
+                this.corrections[3] = "error";
+            }
+        }
+    },
+    computed: {
+        ...mapState(["isLoggedIn", "user"]),
+        progress() {
+            //- check if this.form.password is defined
+            if (!this.password) {
+                return 0;
+            }
+            return Math.min(100, this.password.length * 6);
+        },
+        color() {
+            return ["error", "warning", "success"][Math.floor(this.progress / 40)];
+        }
     }
 };
 </script>
