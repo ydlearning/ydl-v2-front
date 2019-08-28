ef4c441d037c0c05ed58f0a579d4225733b0a918
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-28 23:37:55 +0200

    refactor(thedashboardlist): outsource dashboardlist into a new component TheDashboardList

---
+++ src/components/TheDashboardList.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    v-list(dense)
+        //- My Dashboard
+        v-list-item(to="/dashboard")
+            v-list-item-action
+                v-icon mdi-home
+            v-list-item-content
+                v-list-item-title My dashboard
+
+        template(v-if="isLoggedIn")
+            //- My courses
+            v-list-item(to="/mycourses")
+                v-list-item-action
+                    v-icon mdi-folder-account-outline
+                v-list-item-content
+                    v-list-item-title [My courses]
+                    
+        //- All Courses
+        v-list-item(to="/courses")
+            v-list-item-action
+                v-icon mdi-folder-multiple-outline
+            v-list-item-content
+                v-list-item-title All courses
+
+        //- Calendar
+        v-list-item(to="/calendar")
+            v-list-item-action
+                v-icon mdi-calendar-account-outline
+            v-list-item-content
+                v-list-item-title Calendar
+
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+    name: "TheDashboardList",
+    computed: mapState(["isLoggedIn"])
+};
+</script>
