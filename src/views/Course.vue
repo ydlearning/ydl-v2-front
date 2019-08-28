f26c853950c6612d24384f2d60d2bad0323bf9fc
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-28 23:55:16 +0200

    refactor(course): add course component

---
+++ src/views/Course.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    TheCourse
+</template>
+
+<script>
+import TheCourse from "@/components/TheCourse";
+
+export default {
+    name: "Course",
+    components: {
+        TheCourse
+    }
+};
+</script>
