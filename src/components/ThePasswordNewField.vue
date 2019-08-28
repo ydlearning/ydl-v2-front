61428502518c57853303fc6c07d8cb3cfee83453
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-08-28 19:07:23 +0200

    fix(thepasswordnewfield): fix bug that you could click aplly changes if there is an error

--- src/components/ThePasswordNewField.vue
+++ src/components/ThePasswordNewField.vue
@@ -3,22 +3,22 @@ div
 	//- Password New / Repeat:
 	//- @v-validate: required
 	//- @required: true
-	v-text-field(
-		v-model="password"
+	v-text-field(	
 		v-on:input="check_password"
 		v-validate="{ required: true, min: minCounterPassword, max: maxCounterPassword, regex: regexExpression }"
 		:counter="maxCounterPassword" 
-		name="password"
+		:name="passwordName"
 		:data-vv-as="fieldName"
 		:label="fieldLabel"
-		:class="{'is-danger': errors.has('password')}" 
-		ref="password" 
-		:error-messages="errors.collect('password')"
+		:class="{'is-danger': errors.has(passwordName)}" 
+		:ref="passwordName" 
+		:error-messages="errors.collect(passwordName)"
 		:clearable="fieldClearable"
 		:append-icon=" showPassword ? 'mdi-eye' : 'mdi-eye-off'"
 		@click:append="showPassword = !showPassword"
-		:type="showPassword ? 'text' : 'password'"
+		:type="showPassword ? 'text' : passwordName"
 		@input="$emit('input',$event)"
+		@update:error="hasAnyError"
 	)
 
 	v-progress-linear(
@@ -76,6 +76,11 @@ export default {
             default: "",
             required: true
         },
+        passwordName: {
+            type: String,
+            default: "password",
+            required: false
+        },
         showPassword: {
             type: Boolean,
             default: false,
@@ -103,41 +108,59 @@ export default {
             //- check password length
             if (value.length >= 8) {
                 this.corrections[0] = "success";
+                this.hasAnyError();
             } else {
                 this.corrections[0] = "error";
+                this.hasAnyError();
             }
 
             //- check for character
             if (/[a-zA-Z]/.test(value)) {
                 this.corrections[1] = "success";
+                this.hasAnyError();
             } else {
                 this.corrections[1] = "error";
+                this.$emit("update:error", true);
             }
 
             //- check for number
             if (/\d/.test(value)) {
                 this.corrections[2] = "success";
+                this.hasAnyError();
             } else {
                 this.corrections[2] = "error";
+                this.$emit("update:error", true);
             }
 
             //- check for special character
             if (this.regexExpressionSpecialChars.test(value)) {
                 this.corrections[3] = "success";
+                this.hasAnyError();
                 // [+-@!#%$^?!:.;,()[] {}]
             } else {
                 this.corrections[3] = "error";
+                this.hasAnyError();
             }
+        },
+        hasAnyError() {
+            this.$emit(
+                "errorCheck",
+                this.errors.has(this.passwordName) || !this.corrections.every(x => x === "success")
+            );
         }
     },
+    // [0] === "success" &&
+    //                     this.corrections[1] === "success" &&
+    //                     this.corrections[2] === "success" &&
+    //                     this.corrections[3] === "success")
     computed: {
         ...mapState(["isLoggedIn", "user"]),
         progress() {
             //- check if this.form.password is defined
-            if (!this.password) {
+            if (!this.passwordName) {
                 return 0;
             }
-            return Math.min(100, this.password.length * 6);
+            return Math.min(100, this.passwordName.length * 6);
         },
         color() {
             return ["error", "warning", "success"][Math.floor(this.progress / 40)];
