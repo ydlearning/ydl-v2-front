3f6fea33aaf7b6f97240ebe9195074fb2c9c222d
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-29 01:46:47 +0200

    refactor(course): add overlay to show if the user is new to the course

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,5 +1,15 @@
 <template lang="pug">
-    TheCourse
+    div
+        v-overlay(
+            :value="showWelcomeScreen"
+        )
+            v-card(max-width=400)
+                v-card-title Welcome to the Course!
+                v-card-text Important notes
+                v-card-actions
+                    v-spacer
+                    v-btn(@click="showWelcomeScreen = false") Accept
+        TheCourse
 </template>
 
 <script>
@@ -7,6 +17,12 @@ import TheCourse from "@/components/TheCourse";
 
 export default {
     name: "Course",
+    props: {
+        showWelcomeScreen: {
+            type: Boolean,
+            default: true
+        }
+    },
     components: {
         TheCourse
     }
