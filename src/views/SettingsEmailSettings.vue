645106e58c19eecb064db0536447007ebab29c9e
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-08-29 00:05:55 +0200

    refactor(settingsemailsettings): outsource relevant components

--- src/views/SettingsEmailSettings.vue
+++ src/views/SettingsEmailSettings.vue
@@ -1,73 +1,169 @@
 <template lang="pug">
-    v-container(align-center justify-center)
-        v-card(flat color="transparent")
-            //- v-container
-            //-     v-flex
-            //-         h3.text-center.title.text-center.font-weight-regular 
-            //-         //- p sdjhasjdh
-            //- TODO: .text-center
-            v-toolbar(color='primary' flat)
-                v-toolbar-title {{ this.$route.name }}
-            v-card-text
-                v-form
-                    //- E-mail: Current
-                    //- @v-validate: required | max:100 | min:8
-                    //- @required: true
-                    v-text-field(
-                        v-model='user.email.adress' 
-                        v-validate="'required|email|max:100'" 
-                        :error-messages="errors.collect('email')" 
-                        label='Current E-mail' 
-                        data-vv-name='email' 
-                        required
-                        disabled)
+	v-container(align-center justify-center)
+		v-card(flat color="transparent")
+			//- Snackbar - Info Apply changes successful!
+			v-snackbar(
+				v-model="snackbar"  
+				top 
+				right 
+				color="success")
+					span Changes apply successfully!
+					v-icon(dark) mdi-check-circle
+			//- Snackbar - Info Cancel, form cleared!
+			v-snackbar(
+				v-model="snackbarError"  
+				top 
+				right 
+				color="error")
+					span Cancel, form cleared!
+					v-icon(dark) mdi-alert
+			v-toolbar(color='primary' flat)
+				v-toolbar-title {{ this.$route.name }}
+			v-card-text
+				v-form(ref="form" @submit.prevent="submit")
+					//- E-mail: Current
+					//- @v-validate: required | max:100 | min:8
+					//- @required: true
+					//- v-text-field(
+						v-model='user.email.address' 
+						v-validate="'required|email|max:100'" 
+						:error-messages="errors.collect('email')" 
+						label='Current E-mail' 
+						data-vv-name='email' 
+						required
+						readonly
+					//- )
 
-                    //- E-mail: new
-                    //- @v-validate: required | max:100 | min:8
-                    //- @required: true
-                    v-text-field(
-                        v-model='newEmail' 
-                        v-validate="'required|email|max:100'" 
-                        :error-messages="errors.collect('email')" 
-                        label='New E-mail' 
-                        data-vv-name='email' 
-                        required
-                        clearable
-                        ref="newEmail")
+					//- v-model="user.email.address"
+					TheEmailNewField(
+						v-model="user.email.address"
+						:maxCounterEmail="100"
+						fieldName="Current E-mail"
+						fieldLabel="Current E-mail"
+						@errorCheck="emailNewHasErrors=$event"
+						:fieldReadonly="true"
+						:fieldPlaceholder="user.email.address"
+						:copySymbol="false"
+						fieldId="1"
+					)
 
-                    //- E-mail: repeat
-                    //- @v-validate: required | max:100 | min:8
-                    //- @required: true
-                    v-text-field(
-                        v-validate="'required|confirmed:newEmail'" 
-                        name="email_confirmation" 
-                        :class="{'is-danger': errors.has('email_confirmation')}" 
-                        label='New E-mail' 
-                        data-vv-as='email' 
-                        required
-                        clearable)
-                    span.help.is-danger(
-                        v-show="errors.has('email_confirmation')") {{ errors.first('email_confirmation') }} 
-                v-card-actions
-                    v-tooltip(bottom)
-                        template(v-slot:activator="{ on }")
-                            v-btn(color='primary' v-on="on" @click="") Change E-mail
-                        span A verification email will be sent to your new E-mail adress
+					//- E-mail: new
+					//- @v-validate: required | max:100 | min:8
+					//- @required: true
+					//- v-text-field(
+						v-model='newEmail' 
+						v-validate="'required|email|max:100'" 
+						:error-messages="errors.collect('email')" 
+						label='New E-mail' 
+						data-vv-name='email' 
+						required
+						clearable
+						ref="newEmail")
+						
+					TheEmailNewField(
+						v-model="form.emailNew"
+						:maxCounterEmail="100"
+						fieldName="New E-mail"
+						fieldLabel="New E-mail"
+						@errorCheck="emailNewHasErrors=$event"
+						:copySymbol="false"
+						fieldId="2"
+					)
 
-                    
+					//- E-mail: repeat
+					//- @v-validate: required | max:100 | min:8
+					//- @required: true
+					//- v-text-field(
+					//-     v-validate="'required|confirmed:newEmail'" 
+					//-     name="email_confirmation" 
+					//-     :class="{'is-danger': errors.has('email_confirmation')}" 
+					//-     label='New E-mail' 
+					//-     data-vv-as='email' 
+					//-     required
+					//-     clearable)
+					//- span.help.is-danger(
+					//-     v-show="errors.has('email_confirmation')") {{ errors.first('email_confirmation') }} 
+
+					TheEmailRepeatField(
+						v-model="form.emailRepeat"
+						:maxCounterEmail="maxCounterEmail"
+						fieldName="Repeat E-mail"
+						fieldLabel="Repeat E-mail"
+						:confirmationField="form.emailNew"
+						@errorCheck="emailRepeatHasErrors=$event"
+					)
+
+					v-card-actions
+						v-tooltip(bottom color="red")
+							template(v-slot:activator="{ on: tooltip }")
+								v-btn(
+									color="primary"
+									@click="resetForm"
+									outlined
+									v-on="{ ...tooltip }"
+									) Cancel
+							span
+								v-icon(size="15px") mdi-alert
+								|  All Fields will be deleted!
+						v-spacer
+						v-btn(
+							color="primary"
+							type="submit"
+							@click=""
+							:disabled="!formIsValid"
+							) Apply changes
 </template>
 
 <script>
 import { mapState } from "vuex";
+import TheEmailNewField from "@/components/TheEmailNewField";
+import TheEmailRepeatField from "@/components/TheEmailRepeatField";
 
 export default {
     data() {
+        const defaultForm = Object.freeze({
+            emailNew: "",
+            emailRepeat: ""
+        });
         return {
-            newEmail: ""
+            form: Object.assign({}, defaultForm),
+            defaultForm,
+            newEmail: "",
+            maxCounterEmail: 100,
+            snackbar: false,
+            snackbarError: false
         };
     },
-    computed: mapState(["isLoggedIn", "user"]),
-    components: {}
+    computed: {
+        ...mapState(["isLoggedIn", "user"]),
+        formIsValid() {
+            return (
+                //- check if value ist set
+                this.form.emailNew &&
+                this.form.emailRepeat &&
+                //- check if all fields havn't errors
+                !this.emailNewHasErrors &&
+                !this.emailRepeatHasErrors
+            );
+        }
+    },
+    methods: {
+        //- resets the complete form
+        resetForm() {
+            this.snackbarError = true;
+            this.form = Object.assign({}, this.defaultForm);
+            this.$refs.form.reset();
+            // this.form.emailNew = null;
+            // this.form.emailRepeat = "";
+        },
+
+        //- submit form fields
+        submit() {
+            this.snackbar = true;
+            // this.resetForm(); // resets form after submit
+        }
+    },
+    components: { TheEmailNewField, TheEmailRepeatField }
 };
 </script>
 
