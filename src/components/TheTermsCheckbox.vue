f7a899a71957bf419d562472cec6d0c8a0b708ce
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-08-28 19:30:28 +0200

    refactor(thetermscheckbox): outsource code to this component

---
+++ src/components/TheTermsCheckbox.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+v-checkbox(
+    color="success"
+    @change="$emit('change',$event)"    
+)
+    template(v-slot:label)
+            div(@click.stop="")
+                | Do you accept the 
+                a(href="javascript:;") terms
+                |  and 
+                a(href="javascript:;") conditions
+                | ?
+</template>
+
+<script>
+export default {
+    name: "TheTermsCheckbox",
+    model: {
+        prop: "checked",
+        event: "change"
+    },
+    props: {
+        checked: {
+            type: Boolean,
+            default: false
+        }
+    }
+};
+</script>
+
+<style lang="sass"></style>
