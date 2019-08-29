bb631c91bf0f844e6cf6bd8bd5920d27875e37e7
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-29 18:09:34 +0200

    refactor(resourcedownloaddialog): add file type filter to choose under the advance settings tab

--- src/components/ResourceDownloadDialog.vue
+++ src/components/ResourceDownloadDialog.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-    v-dialog(v-model="showDialog" :max-width="maxWidth")
+    v-dialog(v-model="showDialog" :max-width="maxWidth" max-height="430" min-height="400")
         template(v-slot:activator="{ on }")
             v-btn(icon small v-on="on")
                 v-icon mdi-download-multiple
@@ -8,11 +8,11 @@
                 span Select Resources
                 v-spacer
                 v-card-actions
-                    v-checkbox(@change="checkboxChanged") sd
+                    v-checkbox(v-model="isSelectAllSelected" @change="checkboxChanged" :label="isSelectAllSelected ? 'Deselect All' : 'Select All'")
             v-divider
             v-list
                 v-list-item-group(multiple v-model="selected")
-                    v-list-item(v-for="resource in resources" :key="resource.name")
+                    v-list-item(v-for="resource in filteredResources" :key="resource.name")
                         template(v-slot:default="{ active, toggle }")
                             v-list-item-icon
                                 v-icon {{ fileTypeIconMapping[resource.fileType] }}
@@ -25,10 +25,22 @@
             v-card-actions
                 v-btn(text @click="closeDialog") Close
                 v-spacer
+                v-btn(icon @click="showAdvancedSettings = !showAdvancedSettings")
+                    v-icon mdi-settings
                 v-badge(overlap :color="selected.length > 0 ? 'success' : 'error'")
                     template(v-slot:badge)
                         span {{ selected.length }}
                     v-btn Download
+            v-divider
+            v-row(no-gutters)
+                v-col.pa-4
+                    span.subheading Filter by File Type:
+                    v-chip-group(v-model="selectedFileTypes" multiple active-class="success black--text" column)
+                        v-chip(v-for="resource in resources" :key="resource.fileType" :value="resource.fileType") {{ resource.fileType }}
+                    span.subheading Filter by Type:
+                    v-chip-group(v-model="selectedTypes" multiple active-class="success black--text" column)
+                        v-chip(v-for="resource in resources") {{ resource.type }}
+
 </template>
 
 <script>
@@ -61,6 +73,11 @@ export default {
                     name: "Lecture 01",
                     type: "lecture",
                     fileType: "pdf"
+                },
+                {
+                    name: "Lecture 01 Video",
+                    type: "moviesdasd",
+                    fileType: "movie"
                 }
             ]
         }
@@ -68,13 +85,27 @@ export default {
     data() {
         return {
             showDialog: false,
-            selected: []
+            selected: [],
+            isSelectAllSelected: false,
+            showAdvancedSettings: true,
+            selectedFileTypes: [],
+            selectedTypes: []
         };
+    },
+    computed: {
+        filteredResources() {
+            if (this.selectedFileTypes.length === 0) {
+                return this.resources;
+            }
+
+            return this.resources.filter(x => this.selectedFileTypes.includes(x.fileType));
+        }
     },
     methods: {
         closeDialog() {
             this.showDialog = false;
             this.selected = [];
+            this.isSelectAllSelected = false;
         },
         selectAll() {
             this.selected = Array.from(Array(this.resources.length).keys());
@@ -82,6 +113,9 @@ export default {
         checkboxChanged(value) {
             value ? this.selectAll() : (this.selected = []);
         }
+    },
+    mounted() {
+        this.showDialog = true;
     }
 };
 </script>
