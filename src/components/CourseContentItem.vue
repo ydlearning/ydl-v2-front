994a6e737a39116065e70686647e22aeee3df221
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-31 15:00:58 +0200

    refactor(coursecontentitem): outsource the section of content into a new component

---
+++ src/components/CourseContentItem.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+v-card(flat)
+    v-card-text
+        v-toolbar(dense)
+            v-toolbar-title {{ title }}
+            v-spacer
+            ResourceDownloadDialog
+        v-card-text
+            CourseContentItemContent
+        v-divider
+        CourseContentItemResourcesList
+</template>
+
+<script>
+import ResourceDownloadDialog from "@/components/ResourceDownloadDialog";
+import CourseContentItemContent from "@/components/CourseContentItemContent";
+import CourseContentItemResourcesList from "@/components/CourseContentItemResourcesList";
+
+export default {
+    name: "CourseContentItem",
+    props: {
+        title: {
+            type: String,
+            default: "<title>"
+        }
+    },
+    components: {
+        CourseContentItemContent,
+        CourseContentItemResourcesList,
+        ResourceDownloadDialog
+    }
+};
+</script>
