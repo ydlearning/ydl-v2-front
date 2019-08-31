56ef24ba7aece13b0d0f734f6455da1b448328a6
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-31 14:59:35 +0200

    refactor(coursecontentitemresourcelist): outsource resourcelist into new component

---
+++ src/components/CourseContentItemResourcesList.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+    v-list(dense)  
+        v-list-item-group
+            v-list-item(v-for="resource in resources" :key="resource.name")
+                v-list-item-icon
+                    v-icon {{ fileTypeIconMapping[resource.fileType] }}
+                v-list-item-content
+                    v-list-item-title {{ resource.name }}
+</template>
+
+<script>
+export default {
+    name: "CourseContentItemResourcesList",
+    props: {
+        fileTypeIconMapping: {
+            type: Object,
+            //- default with Array/Object must be a function
+            default: () => ({
+                pdf: "mdi-file-pdf",
+                movie: "mdi-file-video",
+                //- TODO: just workaround
+                null: "mdi-forum"
+            })
+        },
+        resources: {
+            type: Array,
+            default: () => [
+                {
+                    name: "Announcements",
+                    type: "forum",
+                    fileType: null
+                },
+                {
+                    name: "Studentforum",
+                    type: "forum",
+                    fileType: null
+                },
+                {
+                    name: "Script",
+                    type: "script",
+                    fileType: "pdf"
+                }
+            ]
+        }
+    }
+};
+</script>
+
+<style lang="sass"></style>
