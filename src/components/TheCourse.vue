36f192c1b82cc37675e403e420528e83cc6ea177
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-08-31 15:01:49 +0200

    refactor(thecourse): use component CourseContentItem

--- src/components/TheCourse.vue
+++ src/components/TheCourse.vue
@@ -7,95 +7,21 @@
                     v-spacer
                     ResourceDownloadDialog
                 v-card-text
-                    v-card(flat)
-                        v-card-text
-                            v-toolbar(dense)
-                                v-toolbar-title Announcements
-                                v-spacer
-                                ResourceDownloadDialog
-                            v-card-text
-                                //- template text which will be editable by the course admin
-                                div.warning.black--text fetch this data from database later
-                                h4#lecture- Lecture:
-                                ul
-                                    li
-                                        | Thursday from 10:00 - 12:00 
-                                        strong Room 453 Main
-                                    li
-                                        strong Start at 19.08.2019
-                                h4#tutorials- Tutorials:
-                                ul
-                                    li
-                                        | Monday from 17:00 - 18:00 
-                                        strong Room 292
-                                    li Friday from 20:00 - 12:00
-                                //- template text end
-                            v-divider
-                            v-list(dense)
-                                v-list-item-group
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-forum
-                                        v-list-item-content
-                                            v-list-item-title Announcements
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-forum
-                                        v-list-item-content
-                                            v-list-item-title Studentforum
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-file-pdf
-                                        v-list-item-content
-                                            v-list-item-title Script
-                    v-card(flat)
-                        v-card-text
-                            v-toolbar(dense)
-                                v-toolbar-title Week 19.08 - 25.08
-                                v-spacer
-                                ResourceDownloadDialog
-                            v-card-text
-                                div empty
-                            v-divider
-                            v-list(dense)
-                                v-list-item-group
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-file-pdf
-                                        v-list-item-content
-                                            v-list-item-title Lecture 00 (Introduction)
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-map-marker-question
-                                        v-list-item-content
-                                            v-list-item-title Quiz 01 (Raytrace)
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-file-pdf
-                                        v-list-item-content
-                                            v-list-item-title Helper
-                                    v-list-item
-                                        v-list-item-icon
-                                            v-icon mdi-file-video
-                                        v-list-item-content
-                                            v-list-item-title Lecture 00 (Video)
-                    v-card(flat)
-                        v-card-text
-                            v-toolbar(dense)
-                                v-toolbar-title Week 19.08 - 25.08
-                                v-spacer
-                                ResourceDownloadDialog
-                    
-                            
+                    CourseContentItem(title="Announcements")
+                    CourseContentItem(title="Week 19.08 - 25.08")
+                    CourseContentItem(title="Week 26.08 - 2.09")
+
 </template>
 
 <script>
 import ResourceDownloadDialog from "@/components/ResourceDownloadDialog";
+import CourseContentItem from "@/components/CourseContentItem";
 
 export default {
     name: "TheCourse",
     components: {
-        ResourceDownloadDialog
+        ResourceDownloadDialog,
+        CourseContentItem
     }
 };
 </script>
