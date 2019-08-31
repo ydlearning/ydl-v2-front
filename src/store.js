47b80a4fd17d54ffb9f5713deeb654158cb51b2a
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-01 00:55:06 +0200

    refactor(store): add api mock data for a current course
    the data will be used inside of the TheCourse component

--- src/store.js
+++ src/store.js
@@ -17,6 +17,111 @@ export default new Vuex.Store({
             },
             password: "AbCd1234+-",
             role: "Student"
+        },
+        current_course: {
+            name: "Computergraphics",
+            term: "WS",
+            start: "2019-08-31T22:34:54+0000",
+            end: "2019-10-31T22:34:54+0000",
+            content: {
+                announcement: {
+                    body: {
+                        content: "Thats Content inside the Announcement!",
+                        last_edit: "2019-08-31T22:34:54+0000"
+                    },
+                    resources: [
+                        {
+                            name: "Announcements",
+                            category: "forum",
+                            type: "link"
+                        },
+                        {
+                            name: "Studentforum",
+                            category: "forum",
+                            type: "link"
+                        },
+                        {
+                            name: "Script",
+                            category: "script",
+                            type: "pdf",
+                            uploaded: "2019-08-27T22:34:54+0000",
+                            last_edit: "2019-08-28T22:34:54+0000"
+                        },
+                        {
+                            name: "OpenGL Cheat Sheet (v2)",
+                            category: null,
+                            type: "pdf",
+                            uploaded: "2019-08-29T22:34:54+0000",
+                            last_edit: "2019-08-30T22:34:54+0000"
+                        }
+                    ]
+                },
+                contents: [
+                    {
+                        // calculated based on the start date from the course inside the backend
+                        week: "29.03 - 06.4",
+                        body: null,
+                        resources: [
+                            {
+                                name: "CG Lecture 00: Begin",
+                                category: "lecture",
+                                type: "pdf",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            },
+                            {
+                                name: "CG Lecture 00",
+                                category: "movie",
+                                type: "movie",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            },
+                            {
+                                name: "CG Quiz for Lecture 00",
+                                category: "interactive learning",
+                                type: "quiz",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            }
+                        ]
+                    },
+                    {
+                        // calculated based on the start date from the course inside the backend
+                        week: "12.04 - 19.04",
+                        body: null,
+                        resources: [
+                            {
+                                name: "CG Lecture 01: OpenGL Pipeline",
+                                category: "lecture",
+                                type: "pdf",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            },
+                            {
+                                name: "CG Lecture 01",
+                                category: "movie",
+                                type: "movie",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            },
+                            {
+                                name: "CG Quiz for Lecture 01",
+                                category: "interactive learning",
+                                type: "quiz",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            },
+                            {
+                                name: "Reference Card for OpenGL Pipeline",
+                                category: "cheat sheet",
+                                type: "pdf",
+                                uploaded: "2019-08-29T22:34:54+0000",
+                                last_edit: "2019-08-29T22:34:54+0000"
+                            }
+                        ]
+                    }
+                ]
+            }
         }
     },
     mutations: {
