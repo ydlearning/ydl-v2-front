cf9371c601872c73f203ee82c4b1ae335bf08aaa
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-01 01:41:15 +0200

    refactor(thecourse): use date and name variable from mock data

--- src/components/TheCourse.vue
+++ src/components/TheCourse.vue
@@ -3,8 +3,9 @@
         v-col
             v-card(max-width=1200 color="transparent" flat)
                 v-toolbar
-                    v-toolbar-title [18/19 W] Computergraphics
+                    v-toolbar-title [{{ new Date(current_course.start).format("YY") + "/" + new Date(current_course.end).format("YY") + " " + current_course.term }}] {{ current_course.name }}
                     v-spacer
+                    v-toolbar-title.grey--text.ma-4 {{ new Date(current_course.start).format("DD.MM.YY") + " - " + new Date(current_course.end).format("DD.MM.YY") }}
                     ResourceDownloadDialog
                 v-card-text
                     CourseContentItem(title="Announcements")
@@ -14,11 +15,13 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import ResourceDownloadDialog from "@/components/ResourceDownloadDialog";
 import CourseContentItem from "@/components/CourseContentItem";
 
 export default {
     name: "TheCourse",
+    computed: mapState(["current_course"]),
     components: {
         ResourceDownloadDialog,
         CourseContentItem
