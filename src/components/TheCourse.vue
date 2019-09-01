9d93e5ffffe25ce5f8a7c4170361846b4659a1f4
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-01 11:59:40 +0200

    refactor(thecourse): adapt changes from the coursecontentitem component to use names v-slots

--- src/components/TheCourse.vue
+++ src/components/TheCourse.vue
@@ -8,9 +8,40 @@
                     v-toolbar-title.grey--text.ma-4 {{ new Date(current_course.start).format("DD.MM.YY") + " - " + new Date(current_course.end).format("DD.MM.YY") }}
                     ResourceDownloadDialog
                 v-card-text
-                    CourseContentItem(title="Announcements")
-                    CourseContentItem(title="Week 19.08 - 25.08")
-                    CourseContentItem(title="Week 26.08 - 2.09")
+                    CourseContentItem(
+                        title="Announcements"
+                        :hasBody="current_course.content.announcement.body ? true : false"
+                    )
+                        template(v-slot:content)
+                            CourseContentItemContent(
+                                :content="current_course.content.announcement.body.content"
+                            )
+                        template(v-slot:resourceList)
+                            CourseContentItemResourcesList(
+                                :resources="current_course.content.announcement.resources"
+                            )
+                        template(v-slot:resourceDownload)
+                            ResourceDownloadDialog(
+                                :resources="current_course.content.announcement.resources"
+                            )
+                    CourseContentItem(
+                        v-for="content in current_course.content.contents" :key="content.name"
+                        :title="content.week"
+                        :hasBody="content.body ? true : false"
+                    )
+                        template(v-slot:content)
+                            CourseContentItemContent(
+                                v-if="content.body"
+                                :content="content.body"
+                            )
+                        template(v-slot:resourceList)
+                            CourseContentItemResourcesList(
+                                :resources="content.resources"
+                            )
+                        template(v-slot:resourceDownload)
+                            ResourceDownloadDialog(
+                                :resources="content.resources"
+                            )
 
 </template>
 
@@ -18,13 +49,17 @@
 import { mapState } from "vuex";
 import ResourceDownloadDialog from "@/components/ResourceDownloadDialog";
 import CourseContentItem from "@/components/CourseContentItem";
+import CourseContentItemContent from "@/components/CourseContentItemContent";
+import CourseContentItemResourcesList from "@/components/CourseContentItemResourcesList";
 
 export default {
     name: "TheCourse",
     computed: mapState(["current_course"]),
     components: {
         ResourceDownloadDialog,
-        CourseContentItem
+        CourseContentItem,
+        CourseContentItemContent,
+        CourseContentItemResourcesList
     }
 };
 </script>
