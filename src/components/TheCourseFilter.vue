4df9f50898a2cab6484c3c68bdcfd0986b1e4a04
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-02 02:28:27 +0200

    refactor(thecoursefilter): add component thecoursefilter
    thecoursefilter will be used to filter on the mycourses site for specific courses

---
+++ src/components/TheCourseFilter.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+    BaseDashboardItem(
+        title="Filter"
+    )
+        v-card-text
+            v-span.subheading Show only...
+            v-row(justify="space-around")
+                v-switch(label="Favorites")
+                v-switch(label="Active")
+                v-switch(label="Unread")
+            v-span.subheading Filter by Category
+            v-chip-group(column multiple)
+                v-chip(v-for="n in 8" :key="n") {{ "chip" + n }}
+            v-span.subheading Filter by Term
+            v-chip-group(column multiple)
+                v-chip WS
+                v-chip SS          
+</template>
+
+<script>
+import BaseDashboardItem from "@/components/BaseDashboardItem";
+
+export default {
+    name: "TheCourseFilter",
+    components: {
+        BaseDashboardItem
+    }
+};
+</script>
