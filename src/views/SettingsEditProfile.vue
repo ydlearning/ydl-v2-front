544b07358b18c0a7c176c04727e533e0af54886d
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-06 00:25:13 +0200

    refactor(settingseditprofile): add snackbar and buttons component

--- src/views/SettingsEditProfile.vue
+++ src/views/SettingsEditProfile.vue
@@ -1,26 +1,7 @@
 <template lang="pug">
     v-container(align-center justify-center)
         v-card(flat color="transparent")
-            //- div
-                //- alt:
-                //- Snackbar - Info Apply changes successful!
-                v-snackbar(
-                    v-model="snackbarSuccess"  
-                    top 
-                    right 
-                    color="success")
-                        span Changes apply successfully!
-                        v-icon(dark) mdi-check-circle
-                //- Snackbar - Info Cancel, form cleared!
-                v-snackbar(
-                    v-model="snackbarError"  
-                    top 
-                    right 
-                    color="error")
-                        span Cancel, form cleared!
-                        v-icon(dark) mdi-alert
 
-            //- Neu
             TheSettingsSnackbar(
                 :snackbarSuccess='snackbarSuccess'
                 :snackbarError='snackbarError'
@@ -30,73 +11,20 @@
             v-toolbar(color='primary' flat)
                 v-toolbar-title {{ this.$route.name }}
             v-card-text
-                //- Username
-                    //- @v-validate: required | min:3 | max:40 | alpha_dash
-                    //- @required: true
-                    //- @disabled: true
-                    //- v-text-field(
-                        v-model='user.username'
-                        v-validate="'required|min:3|max:40|alpha_dash'" 
-                        :error-messages="errors.collect('username')" 
-                        label='Username' 
-                        data-vv-name='username' 
-                        required
-                        disabled)
-                    
-                //- TheUsernameField(
-                    v-model="user.username"
-                    :minCounter="3"
-                    :maxCounter="40"
-                    fieldName="Username"
-                    fieldLabel="Username"
-                    @errorCheck="usernameHasErrors=$event"
-                    :fieldReadonly="true"
-                    :fieldPlaceholder="user.username"
-                    :copySymbol="false")
-
-                //- v-select(
-                    v-model='user.role' 
-                    v-validate="'required'" 
-                    :items='items' 
-                    :error-messages="errors.collect ('select')" 
-                    label='Role' 
-                    data-vv-name='select' 
-                    required
-                    :menu-props="{ top: true, offsetY: true }"
-                    disabled=false)
-                    //- maybe diabled?    
-
-                //- TheRoleSelect(
-                    v-model="user.role"
-                    :fieldItems='items'
-                    fieldName="Role"
-                    fieldLabel="Role"
-                    :fieldDisable="false"
-                    @errorCheck="roleHasErrors=$event")
                 
                 v-text-field(
                     v-model='user.name'
-                    readonly=true
-                    disabled=false
+                    readonly=false
+                    disabled=true
                     label="Username")
 
                 v-text-field(
                     v-model='user.role'
                     readonly=false
-                    disabled
+                    disabled=true
                     label="Role")
 
                 v-form(ref="form" @submit.prevent="submit")
-                    //- Full name:
-                    //- @v-validate: required | max:50 | alpha_spaces
-                    //- @required: true
-                    //- v-text-field(
-                        v-model='user.name' 
-                        v-validate="'required|max:50|alpha_spaces'" 
-                        :error-messages="errors.collect('name')" 
-                        label='Full Name' 
-                        data-vv-name='name' 
-                        required)
                     
                     TheFullNameField(
                         v-model="form.fullname"
@@ -105,33 +33,11 @@
                         fieldName="Full name"
                         fieldLabel="Full name"
                         @errorCheck="fullNameHasErrors=$event")
-                    
-                    //- v-card-actions
-                        //- div
-                            v-tooltip(bottom color="red")
-                                template(v-slot:activator="{ on: tooltip }")
-                                    v-btn(
-                                        color="primary"
-                                        @click="resetForm"
-                                        outlined
-                                        v-on="{ ...tooltip }"
-                                        ) Cancel 
-                                span
-                                    v-icon(size="15px") mdi-alert
-                                    |  All Fields will be deleted!
-                            v-spacer
-                            v-btn(
-                                color="primary"
-                                type="submit"
-                                @click=""
-                                :disabled="!formIsValid"
-                                ) Apply changes
 
                     TheSettingsButtons(
                         @cancel="resetForm"
                         @apply="submit"
-                        :formIsValid="formIsValid"
-                    )
+                        :formIsValid="formIsValid")
 </template>
 
 <script>
@@ -156,9 +62,7 @@ export default {
             snackbarSuccess: false,
             snackbarError: false,
             defaultForm,
-            usernameHasErrors: false,
             fullNameHasErrors: false,
-            roleHasErrors: false
         };
     },
     computed: {
