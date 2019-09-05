9abba925ed97ebe3d1added5947f24a10a8497ed
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-05 23:27:41 +0200

    refactor(thesettingsbuttons): outsource card buttons

---
+++ src/components/TheSettingsButtons.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+v-card-actions
+    v-tooltip(bottom color="red")
+        template(v-slot:activator="{ on: tooltip }")
+            v-btn(
+                color="primary"
+                @click="$emit('cancel')"
+                outlined
+                v-on="{ ...tooltip }"
+                ) Cancel
+        span
+            v-icon(size="15px") mdi-alert
+            |  All Fields will be deleted!
+    v-spacer
+    v-btn(
+        color="primary"
+        type="submit"
+        @click="$emit('apply')"
+        :disabled="!formIsValid"
+    ) Apply changes
+</template>
+
+<script>
+export default {
+    name: "TheSettingsButtons",
+    props: {
+        value: {
+            type: String,
+            default: ""
+        },
+        formIsValid: {
+            type: Boolean,
+            default: false,
+            required: false
+        }
+    },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="sass"></style>
