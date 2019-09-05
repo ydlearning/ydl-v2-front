aea7dcd5a0088d27b90fe080143e82ea74b5f63b
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-06 00:25:29 +0200

    refactor: add snackbar and buttons component

--- src/views/SettingsEmailSettings.vue
+++ src/views/SettingsEmailSettings.vue
@@ -1,65 +1,34 @@
 <template lang="pug">
 	v-container(align-center justify-center)
 		v-card(flat color="transparent")
-			//- Snackbar - Info Apply changes successful!
-			v-snackbar(
-				v-model="snackbar"  
-				top 
-				right 
-				color="success")
-					span Changes apply successfully!
-					v-icon(dark) mdi-check-circle
-			//- Snackbar - Info Cancel, form cleared!
-			v-snackbar(
-				v-model="snackbarError"  
-				top 
-				right 
-				color="error")
-					span Cancel, form cleared!
-					v-icon(dark) mdi-alert
+
+			TheSettingsSnackbar(
+				:snackbarSuccess='snackbarSuccess'
+				:snackbarError='snackbarError'
+				@snackbarRemoveSuccess="snackbarSuccess=false"
+				@snackbarRemoveError="snackbarError=false")
+
 			v-toolbar(color='primary' flat)
 				v-toolbar-title {{ this.$route.name }}
 			v-card-text
-				//- Current E-Mail field
-				TheEmailNewField(
-					v-model="user.email.address"
-					:maxCounterEmail="100"
-					fieldName="Current E-mail"
-					fieldLabel="Current E-mail"
-					@errorCheck="emailNewHasErrors=$event"
-					:fieldReadonly="true"
-					:fieldPlaceholder="user.email.address"
-					:copySymbol="false"
-					fieldId="1"
-				)
+
+				v-text-field(
+					v-model='user.email.address'
+					readonly=false
+					disabled=true
+					label="Current E-mail")
 
 				v-form(ref="form" @submit.prevent="submit")
 						
 					TheEmailNewField(
 						v-model="form.emailNew"
-						:maxCounterEmail="100"
+						:maxCounterEmail="maxCounterEmail"
 						fieldName="New E-mail"
 						fieldLabel="New E-mail"
 						@errorCheck="emailNewHasErrors=$event"
 						:copySymbol="false"
 						fieldId="2"
 					)
-
-					
-
-					//- E-mail: repeat
-					//- @v-validate: required | max:100 | min:8
-					//- @required: true
-					//- v-text-field(
-					//-     v-validate="'required|confirmed:newEmail'" 
-					//-     name="email_confirmation" 
-					//-     :class="{'is-danger': errors.has('email_confirmation')}" 
-					//-     label='New E-mail' 
-					//-     data-vv-as='email' 
-					//-     required
-					//-     clearable)
-					//- span.help.is-danger(
-					//-     v-show="errors.has('email_confirmation')") {{ errors.first('email_confirmation') }} 
 
 					TheEmailRepeatField(
 						v-model="form.emailRepeat"
@@ -70,77 +39,68 @@
 						@errorCheck="emailRepeatHasErrors=$event"
 					)
 
-					v-card-actions
-						v-tooltip(bottom color="red")
-							template(v-slot:activator="{ on: tooltip }")
-								v-btn(
-									color="primary"
-									@click="resetForm"
-									outlined
-									v-on="{ ...tooltip }"
-									) Cancel
-							span
-								v-icon(size="15px") mdi-alert
-								|  All Fields will be deleted!
-						v-spacer
-						v-btn(
-							color="primary"
-							type="submit"
-							@click=""
-							:disabled="!formIsValid"
-							) Apply changes
+					TheSettingsButtons(
+						@cancel="resetForm"
+						@apply="submit"
+						:formIsValid="formIsValid")
 </template>
 
 <script>
 import { mapState } from "vuex";
 import TheEmailNewField from "@/components/TheEmailNewField";
 import TheEmailRepeatField from "@/components/TheEmailRepeatField";
+import TheSettingsButtons from "@/components/TheSettingsButtons";
+import TheSettingsSnackbar from "@/components/TheSettingsSnackbar";
 
 export default {
-    data() {
-        const defaultForm = Object.freeze({
-            emailNew: "",
-            emailRepeat: ""
-        });
-        return {
-            form: Object.assign({}, defaultForm),
-            defaultForm,
-            newEmail: "",
-            maxCounterEmail: 100,
-            snackbar: false,
-            snackbarError: false
-        };
-    },
-    computed: {
-        ...mapState(["isLoggedIn", "user"]),
-        formIsValid() {
-            return (
-                //- check if value ist set
-                this.form.emailNew &&
-                this.form.emailRepeat &&
-                //- check if all fields havn't errors
-                !this.emailNewHasErrors &&
-                !this.emailRepeatHasErrors
-            );
-        }
-    },
-    methods: {
-        //- resets the complete form
-        resetForm() {
-            this.snackbarError = true;
-            this.form = Object.assign({}, this.defaultForm);
-            this.$refs.form.reset();
-            // this.form.emailNew = null;
-            // this.form.emailRepeat = "";
-        },
+	data() {
+		const defaultForm = Object.freeze({
+			emailNew: "",
+			emailRepeat: ""
+		});
+		return {
+			form: Object.assign({}, defaultForm),
+			defaultForm,
+			newEmail: "",
+			maxCounterEmail: 100,
+			snackbarSuccess: false,
+			snackbarError: false,
+			emailNewHasErrors: false,
+			emailRepeatHasErrors: false
+		};
+	},
+	computed: {
+		...mapState(["isLoggedIn", "user"]),
+		formIsValid() {
+			return (
+				//- check if value ist set
+				this.form.emailNew !== "" &&
+				this.form.emailRepeat !== "" &&
+				//- check if all fields havn't errors
+				!this.emailNewHasErrors &&
+				!this.emailRepeatHasErrors
+			);
+		}
+	},
+	methods: {
+		//- resets the complete form
+		resetForm() {
+			this.snackbarError = true;
+			this.form = Object.assign({}, this.defaultForm);
+			this.$refs.form.reset();			
+		},
 
-        //- submit form fields
-        submit() {
-            this.snackbar = true;
-            // this.resetForm(); // resets form after submit
-        }
-    },
-    components: { TheEmailNewField, TheEmailRepeatField }
+		//- submit form fields
+		submit() {
+			this.snackbarSuccess = true;
+			// this.resetForm(); // resets form after submit
+		}
+	},
+	components: { 
+		TheEmailNewField, 
+		TheEmailRepeatField,
+		TheSettingsButtons,
+		TheSettingsSnackbar }
 };
 </script>
 
