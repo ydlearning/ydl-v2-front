48ca77b396e3f4485bf192d99b69c98c8494159d
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-06 00:24:42 +0200

    refactor(settingschangepassword): add snackbar and buttons component

--- src/views/SettingsChangePassword.vue
+++ src/views/SettingsChangePassword.vue
@@ -1,22 +1,13 @@
 <template lang="pug">
 	v-container(align-center justify-center)
 		v-card(flat color="transparent")
-			//- Snackbar - Info Apply changes successful!
-			v-snackbar(
-				v-model="snackbar"  
-				top 
-				right 
-				color="success")
-					span Changes apply successfully!
-					v-icon(dark) mdi-check-circle
-			//- Snackbar - Info Cancel, form cleared!
-			v-snackbar(
-				v-model="snackbarError"  
-				top 
-				right 
-				color="error")
-					span Cancel, form cleared!
-					v-icon(dark) mdi-alert
+		
+			TheSettingsSnackbar(
+				:snackbarSuccess='snackbarSuccess'
+				:snackbarError='snackbarError'
+				@snackbarRemoveSuccess="snackbarSuccess=false"
+				@snackbarRemoveError="snackbarError=false")
+
 			v-toolbar(color='primary' flat)
 				v-toolbar-title {{ this.$route.name }}
 			v-card-text
@@ -63,26 +54,10 @@
 						v-model="form.terms" 
 					)
 
-					v-card-actions
-						v-tooltip(bottom color="red")
-							template(v-slot:activator="{ on: tooltip }")
-								v-btn(
-									color="primary"
-									@click="resetForm"
-									outlined
-									v-on="{ ...tooltip }"
-									) Cancel
-							span
-								v-icon(size="15px") mdi-alert
-								|  All Fields will be deleted!
-						v-spacer
-						v-btn(
-							color="primary"
-							type="submit"
-							@click=""
-							:disabled="!formIsValid"
-							v-on="{ ...tooltip }"
-							) Apply changes
+					TheSettingsButtons(
+						@cancel="resetForm"
+						@apply="submit"
+						:formIsValid="formIsValid")
 </template>
 
 <script>
@@ -90,70 +65,74 @@ import ThePasswordField from "@/components/ThePasswordField";
 import ThePasswordNewField from "@/components/ThePasswordNewField";
 import ThePasswordRepeatField from "@/components/ThePasswordRepeatField";
 import TheTermsCheckbox from "@/components/TheTermsCheckbox";
+import TheSettingsButtons from "@/components/TheSettingsButtons";
+import TheSettingsSnackbar from "@/components/TheSettingsSnackbar";
 import { mapState } from "vuex";
 
 export default {
-    name: "SettingsChangePassword",
-    data() {
-        const defaultForm = Object.freeze({
-            password: "",
-            passwordSet: "",
-            passwordRepeat: "",
-            terms: false
-        });
-        return {
-            form: Object.assign({}, defaultForm),
-            minCounterPassword: 8,
-            maxCounterPassword: 128,
-            regexExpression: /^([a-zA-Z0-9$&+,:;=?@#'<>.^*()%!-]+)$/,
-            regexExpressionSpecialChars: /[$&+,:;=?@#'<>.^*()%!-]/,
-            snackbar: false,
-            snackbarError: false,
-            defaultForm,
-            passwordHasErrors: false,
-            passwordSetHasErrors: false,
-            passwordRepeatHasErrors: false
-        };
-    },
-    mounted() {},
-    components: {
-        ThePasswordField,
-        ThePasswordNewField,
-        ThePasswordRepeatField,
-        TheTermsCheckbox
-    },
-    computed: {
-        ...mapState(["isLoggedIn", "user"]),
+	name: "SettingsChangePassword",
+	data() {
+		const defaultForm = Object.freeze({
+			password: "",
+			passwordSet: "",
+			passwordRepeat: "",
+			terms: false
+		});
+		return {
+			form: Object.assign({}, defaultForm),
+			minCounterPassword: 8,
+			maxCounterPassword: 128,
+			regexExpression: /^([a-zA-Z0-9$&+,:;=?@#'<>.^*()%!-]+)$/,
+			regexExpressionSpecialChars: /[$&+,:;=?@#'<>.^*()%!-]/,
+			snackbarSuccess: false,
+			snackbarError: false,
+			defaultForm,
+			passwordHasErrors: false,
+			passwordSetHasErrors: false,
+			passwordRepeatHasErrors: false
+		};
+	},
+	mounted() {},
+	components: {
+		ThePasswordField,
+		ThePasswordNewField,
+		ThePasswordRepeatField,
+		TheTermsCheckbox,
+		TheSettingsButtons,
+		TheSettingsSnackbar
+	},
+	computed: {
+		...mapState(["isLoggedIn", "user"]),
 
-        //- returns true if the form and all fields are valid
-        formIsValid() {
-            return (
-                //- check if value ist set
-                this.form.password &&
-                this.form.passwordSet &&
-                this.form.passwordRepeat &&
-                this.form.terms &&
-                //- check if all fields havn't errors
-                !this.passwordHasErrors &&
-                !this.passwordSetHasErrors &&
-                !this.passwordRepeatHasErrors
-            );
-        }
-    },
-    methods: {
-        //- resets the complete form
-        resetForm() {
-            this.snackbarError = true;
-            this.form = Object.assign({}, this.defaultForm);
-            this.$refs.form.reset();
-        },
+		//- returns true if the form and all fields are valid
+		formIsValid() {
+			return (
+				//- check if value ist set
+				this.form.password !== "" &&
+				this.form.passwordSet !== "" &&
+				this.form.passwordRepeat !== "" &&
+				this.form.terms &&
+				//- check if all fields havn't errors
+				!this.passwordHasErrors &&
+				!this.passwordSetHasErrors &&
+				!this.passwordRepeatHasErrors
+			);
+		}
+	},
+	methods: {
+		//- resets the complete form
+		resetForm() {
+			this.snackbarError = true;
+			this.form = Object.assign({}, this.defaultForm);
+			this.$refs.form.reset();
+		},
 
-        //- submit form fields
-        submit() {
-            this.snackbar = true;
-            // this.resetForm(); // resets form after submit
-        }
-    }
+		//- submit form fields
+		submit() {
+			this.snackbarSuccess = true;
+			// this.resetForm(); // resets form after submit
+		}
+	}
 };
 </script>
 
