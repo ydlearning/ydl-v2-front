8bec13a8c2aa96e3f5128dc65477eff93efebe73
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-07 21:43:21 +0200

    refactor(theloginsnackbar): add login snackbar

---
+++ src/components/TheLoginSnackbar.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+div
+    //- Snackbar - Info - successful!
+    v-snackbar(
+        :value="snackbarSuccess"  
+        top 
+        right 
+        color="success"
+        @input="$emit('snackbarRemoveSuccess')")
+            span {{ successText }} successfully!
+            v-icon(dark) mdi-check-circle
+    //- Snackbar - Info - Error
+    v-snackbar(
+        :value="snackbarError"  
+        top 
+        right 
+        color="error"
+        @input="$emit('snackbarRemoveError')")
+            span {{ errorText }}
+            v-icon(dark) mdi-alert
+</template>
+
+<script>
+export default {
+    name: "TheLoginSnackbar",
+    computed: {},
+    props: {
+        value: {
+            type: String,
+            default: ""
+        },
+        snackbarSuccess: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+        successText: {
+            type: String,
+            default: "",
+            required: true
+        },
+        snackbarError: {
+            type: Boolean,
+            default: false,
+            required: false
+        },
+        errorText: {
+            type: String,
+            default: "",
+            required: false
+        }
+    },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="sass"></style>
