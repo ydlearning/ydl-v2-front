66fff5755cbdbd0051173e47cf0effcded9abff3
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-07 21:41:39 +0200

    refactor(theloginbuttons): source out card buttons

---
+++ src/components/TheLoginButtons.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+v-card-actions
+    //- v-tooltip(bottom color="red")
+        //- template(v-slot:activator="{ on: tooltip }")
+        //- v-on="{ ...tooltip }"
+    v-btn(
+        color="primary"
+        @click="$emit('goto')"
+        outlined
+        :to="goTo"
+        ) Go to {{ goToText }}
+    //- span
+        v-icon(size="15px") mdi-alert
+        |  Go to 
+    v-spacer
+    v-btn(
+        color="primary"
+        type="submit"
+        @click="$emit('action')"
+        :disabled="!formIsValid"
+    ) {{ actionText }}
+</template>
+
+<script>
+export default {
+    name: "TheLoginButtons",
+    props: {
+        value: {
+            type: String,
+            default: ""
+        },
+        goToText: {
+            type: String,
+            default: "",
+            required: true
+        },
+        actionText: {
+            type: String,
+            default: "",
+            required: true
+        },
+        goTo: {
+            type: String,
+            default: "",
+            required: true
+        },
+        formIsValid: {
+            type: Boolean,
+            default: false,
+            required: false
+        }
+    },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="sass"></style>
