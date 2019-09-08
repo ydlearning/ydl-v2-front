17aef6b9dbb67c33d8fdafbf541a2597dd9d9bcd
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-09 00:54:33 +0200

    fix(dashboard): add courses which were removed after the refactoring to components

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,8 @@
     v-container.pa-0(fluid)
         v-row.ma-0
             v-col(cols=12 sm=9)
-                TheCourseList
+                //- slice is just a workaround to simulated favorite courses
+                TheCourseList( :courses="my_courses.slice(0, 8)")
 
             v-col(cols=12 sm=3)
                 TheNotification.mb-4
@@ -14,12 +15,15 @@ import TheNotification from "@/components/TheNotification";
 import TheTask from "@/components/TheTask";
 import TheCourseList from "@/components/TheCourseList";
 
+import { mapState } from "vuex";
+
 export default {
     components: {
         TheNotification,
         TheTask,
         TheCourseList
     },
+    computed: mapState(["my_courses"]),
     mounted() {
         console.debug(this.$route.name);
     }
