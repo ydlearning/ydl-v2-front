690e1c96473860f6a89de5eedabe4c7d79e2e442
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-09 01:37:46 +0200

    refactor(resourceeditdialog): add resourceeditdialog component

---
+++ src/components/ResourceEditDialog.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+v-dialog(v-model="showDialog" :max-width="maxWidth" max-height="430" min-height="400")
+        template(v-slot:activator="{ on }")
+            v-btn(icon small v-on="on")
+                v-icon mdi-pencil
+        v-card
+            v-card-title
+                span Edit
+                v-spacer
+                v-card-actions
+                    v-checkbox(v-model="isSelectAllSelected" @change="checkboxChanged" :label="isSelectAllSelected ? 'Deselect All' : 'Select All'")
+            v-divider
+            v-card-action
+                v-btn.warning(outlined) Delete
+</template>
+
+<script>
+export default {};
+</script>
