519f65940de9b869d167307ac97b79c4f170e552
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-09 14:17:39 +0200

    refactor(resourceeditdialog): add name and file form inputs and provide infos about last changes

--- src/components/ResourceEditDialog.vue
+++ src/components/ResourceEditDialog.vue
@@ -1,14 +1,24 @@
 <template lang="pug">
-    v-dialog(v-model="showDialog" @click:outside="reset()" :max-width="550" max-height="430" min-height="400")
+    v-dialog(v-model="showDialog" @click:outside="reset" :max-width="550" max-height="430" min-height="400")
         template(v-slot:activator="{ on }")
             v-btn(icon small v-on="on")
                 v-icon mdi-pencil
-        v-card
+        v-card(width="100%")
             v-card-title
                 v-list-item
                     v-list-item-icon
                         v-icon mdi-pencil
-                    v-list-item-content {{ resource.name }}
+                    v-list-item-content Edit: {{ resource.name }}
+            v-card-text
+                v-row
+                    v-col(cols=12)
+                        v-form(ref="form" @submit.prevent="submit")
+                            v-text-field(label="Name" :value="resource.name")
+                            v-file-input(v-model="file")
+                    v-col(cols=12)
+                        div First Upload: {{ new Date(resource.uploaded).format("YYYY/MM/DD hh:mm:ss a") }}
+                        div Last Edit: {{ new Date(resource.last_edit).format("YYYY/MM/DD hh:mm:ss a") }}
+
             v-divider
             v-card-actions
                 v-badge(overlap :value="showTimer")
@@ -20,6 +30,9 @@
                                 span Delete
                             v-window-item(:value="false")
                                 span Confirm
+                v-btn.ml-4 Reset
+                v-spacer
+                v-btn Save
 </template>
 
 <script>
@@ -31,7 +44,9 @@ export default {
             showTimer: false,
             timerCount: 5,
             timer: null,
-            showDialog: false
+            showDialog: false,
+            //- workaround to simulate an already existing file
+            file: new File([""], this.resource.name)
         };
     },
     methods: {
@@ -70,7 +85,7 @@ export default {
                 name: "CG Lecture 00: Begin",
                 category: "lecture",
                 type: "pdf",
-                uploaded: "2019-08-29T22:34:54+0000",
+                uploaded: "2019-08-27T20:34:54+0000",
                 last_edit: "2019-08-29T22:34:54+0000",
                 download: "#"
             })
