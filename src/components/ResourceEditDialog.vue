1a0adc0312590f8428a182a4b3f5ff780ca637a7
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-09 02:12:25 +0200

    refactor(resourceeditdialog): add delete confirm toggle button with time indicator

--- src/components/ResourceEditDialog.vue
+++ src/components/ResourceEditDialog.vue
@@ -1,19 +1,74 @@
 <template lang="pug">
-v-dialog(v-model="showDialog" :max-width="maxWidth" max-height="430" min-height="400")
+    v-dialog(v-model="showDialog" :max-width="550" max-height="430" min-height="400")
         template(v-slot:activator="{ on }")
             v-btn(icon small v-on="on")
                 v-icon mdi-pencil
         v-card
             v-card-title
-                span Edit
-                v-spacer
-                v-card-actions
-                    v-checkbox(v-model="isSelectAllSelected" @change="checkboxChanged" :label="isSelectAllSelected ? 'Deselect All' : 'Select All'")
+                v-list-item
+                    v-list-item-icon
+                        v-icon mdi-pencil
+                    v-list-item-content {{ resource.name }}
             v-divider
-            v-card-action
-                v-btn.warning(outlined) Delete
+            v-card-actions
+                v-badge(overlap :value="showTimer")
+                    template(v-slot:badge)
+                        span {{ timerCount }}
+                    v-btn(@click="clickDelete" :class=" shouldAskDelete ? 'error' : 'warning black--text'")
+                        v-window(v-model="shouldAskDelete")
+                            v-window-item(:value="true")
+                                span Delete
+                            v-window-item(:value="false")
+                                span Confirm
 </template>
 
 <script>
-export default {};
+export default {
+    name: "ResourceEditDialog",
+    data() {
+        return {
+            shouldAskDelete: true,
+            showTimer: false,
+            timerCount: 5,
+            timer: null,
+            showDialog: false
+        };
+    },
+    methods: {
+        startAskDeleteTimer() {
+            this.showTimer = true;
+            this.timer = setInterval(() => {
+                this.timerCount--;
+                if (this.timerCount <= 0) {
+                    clearInterval(this.timer);
+                    this.timerCount = 5;
+                    this.shouldAskDelete = true;
+                    this.showTimer = false;
+                }
+            }, 1000);
+        },
+        clickDelete() {
+            if (this.shouldAskDelete) {
+                this.shouldAskDelete = false;
+                this.startAskDeleteTimer();
+            } else {
+                clearInterval(this.timer);
+                this.showDialog = false;
+            }
+        }
+    },
+    props: {
+        resource: {
+            type: Object,
+            default: () => ({
+                name: "CG Lecture 00: Begin",
+                category: "lecture",
+                type: "pdf",
+                uploaded: "2019-08-29T22:34:54+0000",
+                last_edit: "2019-08-29T22:34:54+0000",
+                download: "#"
+            })
+        }
+    }
+};
 </script>
