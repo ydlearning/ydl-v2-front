4ee471fda4447938e7b7771a7969696589693345
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-09-12 21:59:17 +0200

    build(buildsite.sh): add colors and add node_modules/.bin path

--- buildsite.sh
+++ buildsite.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+PATH=$PATH:./node_modules/.bin
+
+bok="\e[48;5;214;30m"
+breset="\e[0m"
+
+printf "$bok -> Run linter:$breset\n"
 pnpm run lint;
+printf "$bok -> Run build:$breset\n"
 pnpm run build;
+printf "$bok -> Run vuese:$breset\n"
 vuese gen;
