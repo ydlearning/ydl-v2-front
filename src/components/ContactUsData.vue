484d6967a2b6e7197242de0b9ab830619fb4a7f0
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-13 14:17:41 +0200

    refactor(contactus): source out contact data

---
+++ src/components/ContactUsData.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+span
+    //- p
+        | {{ getCompanyContactPerson }}
+    p
+        | {{ getCompanyName }}
+    p
+        h3 Address
+        //- | {{ getCompanyStreet }}
+        //- br
+        //- | {{ getCompanyBuilding }}
+        //- br
+        //- | {{ getCompanyPostcode }} {{ getCompanyTown }}
+        //- br
+        | {{ getCompanyState }}
+    //- span
+        h2 Contact us
+        p
+            table
+                tr
+                    td Phone number:
+                    td {{ getCompanyPhoneNumber}}
+                tr
+                    td Fax number:
+                    td {{ getCompanyFaxNumber }}
+                tr
+                    td E-mail:
+                    td {{ getCompanyEmail }}
+</template>
+
+<script>
+export default {
+    name: "ContactUsData",
+    computed: {
+        //- Getter for environment variables
+        getCompanyName() {
+            return process.env.VUE_APP_COMPANY_NAME;
+        },
+        getCompanyStreet() {
+            return process.env.VUE_APP_COMPANY_STREET;
+        },
+        getCompanyPostcode() {
+            return process.env.VUE_APP_COMPANY_POSTCODE;
+        },
+        getCompanyTown() {
+            return process.env.VUE_APP_COMPANY_TOWN;
+        },
+        getCompanyState() {
+            return process.env.VUE_APP_COMPANY_STATE;
+        },
+        getCompanyBuilding() {
+            return process.env.VUE_APP_COMPANY_BUILDING;
+        },
+        getCompanyPhoneNumber() {
+            return process.env.VUE_APP_COMPANY_PHONE_NUMBER;
+        },
+        getCompanyFaxNumber() {
+            return process.env.VUE_APP_COMPANY_FAX_NUMBER;
+        },
+        getCompanyEmail() {
+            return process.env.VUE_APP_COMPANY_EMAIL;
+        },
+        getCompanyContactPerson() {
+            return process.env.VUE_APP_COMPANY_CONTACT_PERSON;
+        }
+    }
+};
+</script>
+
+<style lang="sass"></style>
