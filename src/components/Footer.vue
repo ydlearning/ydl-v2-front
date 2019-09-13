5b5e9aa6cec38db5f41f14eedaf0106ee7bd0f6c
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-13 16:53:55 +0200

    refactor(footer): redesign footer of footer; add to top button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,21 @@
 <template lang="pug">
 //- Footer component
 div 
+    //- Footer
     v-footer(
         dark 
         padless
         width="100%" 
         color="primary"
     )
+        //- Row 1
         v-row.lighten-1.white--text(
             justify="center" 
             no-gutters
             width="100%"
         )
-        
             //- Website links
-            v-btn.my-2(
+            v-btn.my-0(
                 v-for="site in sites"
                 :key="site.id" 
                 color="white"
@@ -30,7 +31,7 @@ div
             width="100%"
         )
             //- Footer Text
-            v-card-text 
+            v-card-text.pa-1
                 //- Social media buttons
                 v-tooltip(
                     bottom
@@ -53,25 +54,41 @@ div
                             ) {{ social.iconName }}
                     span {{ social.name }}
             
-            //- Text footer
+            //- Text footer (optional)
             //- v-card-text.white--text.pt-0
                 | Text
 
             v-divider
 
             //- Year / Copyright
-            v-card-text.white--text
-                | {{ new Date().getFullYear() }} — 
-                strong Y&D Learning
-                //- v-flex.xs12
-                //- v-spacer
-                //- v-btn.ma-0(
-                    fab 
-                    dark
-                    x-small
-                    color="grey" 
-                    @click="toTop")
-                    v-icon mdi-arrow-up-circle-outline
+            v-card-text.pa-0.white--text
+                v-container(fluid)
+                    v-row(no-gutters)
+                        v-col(
+                            cols="12" 
+                            sm="4")
+                            v-card.pa-0(flat)
+                        v-col.text-center(
+                            cols="12" 
+                            sm="4"
+                            justify-center)
+                            v-card.pa-0(flat)
+                                | {{ new Date().getFullYear() }} — 
+                                strong Y&D Learning
+                        v-col(
+                            cols="12" 
+                            sm="4")
+                            v-card.pa-0(flat) 
+                                v-card-actions.pa-0.ma-0
+                                    v-spacer
+                                    v-btn.ma-0(
+                                        dark
+                                        fab
+                                        x-small
+                                        depressed
+                                        color="transparent" 
+                                        @click="toTop")
+                                        v-icon mdi-arrow-up-circle-outline
 </template>
 
 <script>
@@ -97,7 +114,7 @@ export default {
             {
                 id: 4,
                 name: "Contact Us",
-                to: "contact_us"
+                to: "/contact_us"
             }
         ],
         socialMedia: [
@@ -117,7 +134,7 @@ export default {
                 id: 3,
                 name: "Backend",
                 iconName: "mdi-github-circle",
-                href: "https://github.com/ydlearning/ydl-v2-api-gql"
+                href: "https://github.com/ydlearning/ydl-v2-api"
             }
         ]
     }),
@@ -125,6 +142,11 @@ export default {
         value: {
             type: String,
             default: ""
+        }
+    },
+    methods: {
+        toTop() {
+            this.$vuetify.goTo(0);
         }
     }
 };
