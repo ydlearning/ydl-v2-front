af23a69abc36d0dbf01e07c54e53655fd62a1ca4
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-10-13 21:39:03 +0200

    refactor(basefootercomponent): add icon property

--- src/components/BaseFooterItem.vue
+++ src/components/BaseFooterItem.vue
@@ -1,19 +1,25 @@
 <template lang="pug">
 v-container.container-padding(fluid)
-        v-row(align='center' justify='center')
-            v-card.elevation-12(width="60%")
-                //- Toolbar / App Title
-                v-toolbar(color='primary' flat)
-                    v-icon.pr-1 mdi-cookie
-                    v-toolbar-title {{ this.$route.name }}
-                    div.flex-grow-1
-                v-card-text.body-1
-                    slot
+    v-row(align='center' justify='center')
+        v-card.elevation-12(width="85%")
+            //- Toolbar / App Title
+            v-toolbar(color='primary' flat)
+                v-icon.pr-1 {{ icon }}
+                v-toolbar-title {{ this.$route.name }}
+                div.flex-grow-1
+            v-card-text.body-1
+                slot
   
 </template>
 
 <script>
 export default {
-    name: "BaseFooterItem"
+    name: "BaseFooterItem",
+    props: {
+        icon: {
+            type: String,
+            default: null
+        }
+    }
 };
 </script>
