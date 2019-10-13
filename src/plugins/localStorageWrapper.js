f178954317b64f8c6829b26a07a21e5c4c75563e
B4rtware <34386047+B4rtware@users.noreply.github.com>
2019-10-13 17:45:27 +0200

    refactor(localstoragewrapper): add a wrapper for localstorage
    wrapper which makes sure that cookies are only beeing written if cookies were accepted

---
+++ src/plugins/localStorageWrapper.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+
+// used for wrap the guard function arround a function call
+function guard (name, ...options) {
+    if (localStorage.getItem("cookie_accepted") === "true") {
+        if (options) {
+            return localStorage[name](...options);
+        }
+        return localStorage[name]();
+    }
+    console.log("storage access denied!");
+}
+
+// actual wrapper for the localStorage object
+const wrapper = {
+    count: 0,
+    clear() {
+        return guard("clear");
+    },
+    setItem(key, value) {
+        return guard("setItem", key, value);
+    },
+    getItem(key, value) {
+        return guard("getItem", key, value);
+    },
+    key(index) {
+        return guard("key", index);
+    },
+    removeItem(key) {
+        return guard("removeItem", key);
+    }
+};
+
+const LocalStorageWrapper = {
+    install(Vue, options) {
+        Vue.prototype.$localStorage = wrapper;
+    }
+};
+
+export default LocalStorageWrapper;
